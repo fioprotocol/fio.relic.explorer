1233303c058dc7c04137d87b8ca42dac913514f6
andrewEze <andreyv@eze.tech>
2025-03-31 11:19:53 +0300

    DASH-1352. Get price and chain id and set it as props.

--- client/src/components/layout/Layout.tsx
+++ client/src/components/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TopBar from './TopBar';
 import Header from './Header';
 import Footer from './Footer';
@@ -9,14 +9,46 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   const [network, setNetwork] = useState('mainnet');
+  const [price, setPrice] = useState('');
+  const [chainId, setChainId] = useState('-');
 
-  const onNetworkChange = (network: string) => {
+  const onNetworkChange = (network: string): void => {
     setNetwork(network);
   };
 
+  const fetchPrice = async (): Promise<void> => {
+    try {
+      const response = await fetch('https://app.fio.net/api/v1/reg/prices?actual=false');
+      const data = await response.json();
+      setPrice(data.data.pricing.usdtRoe);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const fetchChainId = async (): Promise<void> => {
+    try {
+      const response = await fetch('https://fio.eosusa.io/v1/chain/get_info');
+      const data = await response.json();
+      setChainId(data.chain_id);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchPrice();
+    fetchChainId();
+  }, []);
+
   return (
     <div className="layout">
-      <TopBar selectedNetwork={network} onNetworkChange={onNetworkChange} />
+      <TopBar
+        selectedNetwork={network}
+        onNetworkChange={onNetworkChange}
+        price={price}
+        chainId={chainId}
+      />
       <Header />
       <main className="main-content py-4">{children}</main>
       <Footer />
