6122fcf3a3afa9f681d94a8ee3d5d0a89146c4c0
Oleksii Trukhanov <trukhantech@gmail.com>
2025-04-04 15:16:53 +0300

    DASH-1354 Add Home page layout and data.

---
+++ client/src/components/StatisticComponent/StatisticComponent.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { TransactionChart } from '../TransactionChart';
+import { DataTile, DataItem } from '../common/DataTile';
+import { TransactionDataPoint } from '../TransactionChart/types';
+
+type StatisticComponentProps = {
+  days?: number;
+  stats: DataItem[];
+  chartData: TransactionDataPoint[];
+};
+
+export const StatisticComponent: React.FC<StatisticComponentProps> = ({ stats, chartData }) => {
+  return <DataTile title="Statistics" items={stats} columns={2} layout="multi-column"><TransactionChart chartData={chartData} /></DataTile>;
+};
