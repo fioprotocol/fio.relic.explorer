42175e830db55136ca50fa1b00057b67d1df1267
andrewEze <andreyv@eze.tech>
2025-04-08 10:42:04 +0300

    DASH-1359. Add blocks page component, added routes and refactor links.

---
+++ client/src/pages/BlocksPage/BlocksPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Container from 'src/components/layout/Container';
+
+const BlocksPage: React.FC = () => {
+  return (
+    <Container>
+      <h4 className="my-4">Blocks</h4>
+    </Container>
+  );
+};
+
+export default BlocksPage;
