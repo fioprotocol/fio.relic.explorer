9684b7419d7fd3531d0aaa77fbbc6b6ae798fcda
andrewEze <andreyv@eze.tech>
2025-04-08 11:24:45 +0300

    DASH-1356. Card, table, tile styles edits.

--- client/src/components/layout/CardComponent/CardComponent.tsx
+++ client/src/components/layout/CardComponent/CardComponent.tsx
@@ -7,11 +7,17 @@ type CardComponentProps = {
   children: React.ReactNode;
   title?: string;
   className?: string;
-}
+};
 
 export const CardComponent: FC<CardComponentProps> = ({ children, title, className }) => {
-  return <Card className={`p-4 w-100 mb-0 rounded-4 ${styles.cardComponent} ${className}`}>
-    {title && <Card.Header className={`mb-3 p-0 border-0 bg-transparent ${styles.cardTitle}`}>{title}</Card.Header>}
-    {children}
-  </Card>
+  return (
+    <Card className={`p-4 w-100 mb-0 rounded-4 bg-white ${styles.cardComponent} ${className}`}>
+      {title && (
+        <Card.Header className={`mb-3 p-0 border-0 bg-transparent ${styles.cardTitle}`}>
+          {title}
+        </Card.Header>
+      )}
+      {children}
+    </Card>
+  );
 };
