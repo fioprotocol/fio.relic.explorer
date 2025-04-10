4b1c83a4489625d3a4f336abbf20efdacb06afa8
andrewEze <andreyv@eze.tech>
2025-04-10 16:20:31 +0300

    DASH-1360. Styles changes on block details page, add tx list.

--- client/src/services/transactions.ts
+++ client/src/services/transactions.ts
@@ -1,7 +1,7 @@
 import { Transaction, TransactionResponse, TransactionStats } from '@shared/types/transactions';
 import { apiClient } from './api-client';
 
-export const getTransactions = async (params: { offset: number, limit: number }): Promise<TransactionResponse> => {
+export const getTransactions = async (params: { offset: number, limit: number, block_number: number }): Promise<TransactionResponse> => {
   const response = await apiClient.get<TransactionResponse>('/transactions', { params });
   return response.data;
 };
