27bc82d21ece0d50a54c0f408b7002351bb12731
Oleksii Trukhanov <trukhantech@gmail.com>
2025-04-11 18:47:44 +0300

    DASH-1354 Make ability to load statistics for last records in table.

--- client/src/services/stats.ts
+++ client/src/services/stats.ts
@@ -1,9 +1,9 @@
 import { StatsResponse } from '@shared/types/stats';
 import { apiClient } from './api-client';
 
-export const getStats = async (days: number): Promise<StatsResponse> => {
+export const getStats = async ({ days, useLastRecord }: { days: number, useLastRecord: boolean }): Promise<StatsResponse> => {
   const response = await apiClient.get<StatsResponse>(`/stats`, {
-    params: { days }
+    params: { days, useLastRecord }
   });
   return response.data;
 };
