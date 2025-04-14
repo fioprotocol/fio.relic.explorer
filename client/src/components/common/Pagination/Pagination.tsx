cecaf9510827195838bfca17059075f601b8ff89
andrewEze <andreyv@eze.tech>
2025-04-14 14:43:21 +0300

    Fix pagination next prev

--- client/src/components/common/Pagination/Pagination.tsx
+++ client/src/components/common/Pagination/Pagination.tsx
@@ -14,31 +14,33 @@ export const Pagination: React.FC<UsePaginationDefaultProps> = ({
     <div className="d-flex align-items-center justify-content-center my-3 w-100">
       <div className="d-flex flex-wrap gap-3 w-100 justify-content-center">
         <Button
-          variant={isFirstPage ? "outline-secondary" : "primary"}
+          variant={isFirstPage ? 'outline-secondary' : 'primary'}
           onClick={(): void => goToPage?.(1)}
           disabled={isFirstPage}
         >
           First
         </Button>
         <Button
-          variant={isFirstPage ? "outline-secondary" : "primary"}
-          onClick={(): void => goToPage?.(currentPage ?? 1 - 1)}
+          variant={isFirstPage ? 'outline-secondary' : 'primary'}
+          onClick={(): void => goToPage?.((currentPage ?? 1) - 1)}
           disabled={isFirstPage}
         >
           &lt;
         </Button>
         <div className="d-flex align-self-center justify-content-center">
-          <span>{currentPage ?? 1} of {totalPages || 1}</span>
+          <span>
+            {currentPage ?? 1} of {totalPages || 1}
+          </span>
         </div>
         <Button
-          variant={isLastPage ? "outline-secondary" : "primary"}
-          onClick={(): void => goToPage?.(currentPage ?? 1 + 1)}
+          variant={isLastPage ? 'outline-secondary' : 'primary'}
+          onClick={(): void => goToPage?.((currentPage ?? 1) + 1)}
           disabled={isLastPage}
         >
           &gt;
         </Button>
         <Button
-          variant={isLastPage ? "outline-secondary" : "primary"}
+          variant={isLastPage ? 'outline-secondary' : 'primary'}
           onClick={(): void => goToPage?.(totalPages ?? 1)}
           disabled={isLastPage}
         >
